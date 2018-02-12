a9f4fc4fba4def7b240b5d289f4ea686977d04c9
Guan <guang16@uwosh.edu>
2018-02-12 15:56:05 -0600

    my design of lab2

---
+++ app/src/main/java/com/example/guang16/lab/MainActivity.java
@@ -0,0 +1,23 @@
+package com.example.guang16.lab;
+
+import android.content.res.Configuration;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.WindowManager;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if (getResources().getConfiguration().orientation == Configuration.ORIENTATION_LANDSCAPE)
+        {
+            setContentView(R.layout.activity_landscape);}
+        else {
+            setContentView(R.layout.activity_portrait);
+        }
+        getWindow().setSoftInputMode(
+                WindowManager.LayoutParams.SOFT_INPUT_STATE_HIDDEN);
+        }
+    }
+
